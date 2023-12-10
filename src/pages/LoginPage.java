63cf82804216f79efa223ba60f2481f1b64b3491
SayaliHonrao <sayali.honrao@gmail.com>
2023-12-10 23:13:34 +0530

    Sayali_LogInSetup_10Dec23

--- src/pages/LoginPage.java
+++ src/pages/LoginPage.java
@@ -1,23 +1,108 @@
 package pages;
 
-import org.openqa.selenium.By;
+import java.util.List;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 import base.ControlActions;
+import constants.ConstantPath;
 
 public class LoginPage extends ControlActions {
-	
+	String assessmentName;
+
+	@FindBy(xpath = "//input[@id='userEmail']")
+	WebElement userEmailElement;
+
+	@FindBy(xpath = "//input[@id='userPassword']")
+	WebElement userPasswordElement;
+
+	@FindBy(xpath = "//input[@id='login']")
+	WebElement loginBtnElement;
+
+	@FindBy(xpath = "//div[@aria-label='Login Successfully']")
+	WebElement loginSuccessfulElement;
+
+	@FindBy(xpath = "//div[@aria-label='Incorrect email or password.']")
+	WebElement loginUnSuccessfulElement;
+
+	@FindBy(xpath = "//div[@id='Maulik']")
+	List<WebElement> temp;
+
+	@FindBy(xpath = "//div[text()='*Email is required']")
+	WebElement emailIsRequiredElement;
+
+	@FindBy(xpath = "//div[text()='*Password is required']")
+	WebElement passedIsRequiredElement;
+
+	public LoginPage() {
+		PageFactory.initElements(driver, this);
+	}
+
 	public void login(String email, String password) {
-		System.out.println("STEP 1 - Enter user details and Login");
-		driver.findElement(By.id("userEmail")).sendKeys(email);
-		driver.findElement(By.id("userPassword")).sendKeys(password);
-		driver.findElement(By.id("login")).click();
-	} 
-	
-	public boolean isLoginSuccessfullyDisplayed(){	
-		WebElement loginSuccessfulElement = getElement("XPATH","//div[@aria-label='Login Successfully']",true);
-		return loginSuccessfulElement.isDisplayed();
+
+		/*
+		  System.out.println("STEP: Entered Email address"); //1.
+		 driver.findElement(By.xpath("//input[@id='userEmail']")).sendKeys(email);
+		 2. getElement("XPATH", "//input[@id='userEmail']", false);
+		  userEmailElement.sendKeys(email); */
+		enterUserEmail(email);
+
+		/*
+		  System.out.println("STEP: Entered Password");
+		  driver.findElement(By.xpath("//input[@id='userPassword']")).sendKeys(
+		  password); userPasswordElement.sendKeys(password);
+		*/
+
+		enterPassword(password);
+
+		/*
+		  System.out.println("STEP: Clicked on Login Button");
+		  driver.findElement(By.xpath("//input[@id='login']")).click();
+		  loginBtnElement.click();
+		 */
+		clickOnLoginBtn();
+	}
+
+	public void enterUserEmail(String email) {
+		System.out.println("STEP: Entered Email address");
+		// 1. driver.findElement(By.xpath("//input[@id='userEmail']")).sendKeys(email);
+		// 2. getElement("XPATH", "//input[@id='userEmail']", false);
+		userEmailElement.sendKeys(email);
+	}
+
+	public void enterPassword(String password) {
+		System.out.println("STEP: Entered Password");
+		// driver.findElement(By.xpath("//input[@id='userPassword']")).sendKeys(password);
+		userPasswordElement.sendKeys(password);
+	}
+
+	public void clickOnLoginBtn() {
+		System.out.println("STEP : Click on Login Button");
+		loginBtnElement.click();
+	}
+
+	public boolean isLoginSuccessFullyDisplayed() {
+		/*
+		  waitForElementToBeVisible(loginSuccessfulElement); return
+		  loginSuccessfulElement.isDisplayed();
+		 */
+		return isElementDisplayedWithWait(loginSuccessfulElement, ConstantPath.FAST_WAIT);
+	}
+
+	public boolean isLoginUnSuccessfulElementDisplayed() {
+		return isElementDisplayedWithWait(loginUnSuccessfulElement, ConstantPath.FAST_WAIT);
+	}
+
+	public boolean isEmailRequiredElementDisplayed() {
+		return isElementDisplayed(emailIsRequiredElement);
+	}
+
+	public boolean isPasswordRequiredElementDisplayed() {
+		return isElementDisplayed(passedIsRequiredElement);
+	}
+
+	public String getCurrentURL() {
+		return super.getCurrentURL();
 	}
 }
--- src/testscripts/LoginTest.java
+++ src/testscripts/LoginTest.java
@@ -4,31 +4,115 @@ https://docs.google.com/spreadsheets/d/1XAsOC5KFoI_AOnB9Zwn4AvSSU0pHXyGgoCexc8Pt
 
 package testscripts;
 
-import org.openqa.selenium.WebDriver;
+import java.io.IOException;
+
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import base.ControlActions;
 import pages.LoginPage;
+import utility.ExcelOperations;
 
 public class LoginTest {
-	WebDriver driver;
+	LoginPage loginPage;
 
 	@BeforeMethod
 	public void setup() {
 		ControlActions.launchBrowser();
+		loginPage = new LoginPage();
 	}
 
 	@Test
 	public void loginTest() {
-		LoginPage loginPage = new LoginPage();
 		loginPage.login("sayali.honrao@gmail.com", "Sayali@1441");
-		
-		boolean loginFlag = loginPage.isLoginSuccessfullyDisplayed();
+		boolean loginFlag = loginPage.isLoginSuccessFullyDisplayed();
 		Assert.assertTrue(loginFlag);
-	
+	}
+
+	@Test
+	public void verifyErrorMessages() {
+		System.out.println("STEP - Click on Login button");
+		loginPage.clickOnLoginBtn();
+
+		System.out.println("VERIFY - Email required Error message is visible");
+		boolean emailErrorMessageFlag = loginPage.isEmailRequiredElementDisplayed();
+		Assert.assertTrue(emailErrorMessageFlag);
+
+		System.out.println("VERIFY - Password required Error message is visible");
+		boolean passwordErrorMessageFlag = loginPage.isPasswordRequiredElementDisplayed();
+		Assert.assertTrue(passwordErrorMessageFlag);
+	}
+
+	@Test
+	public void verifyPasswordErrorMessage() {
+		System.out.println("STEP - Enter valid User email");
+		loginPage.enterUserEmail("sayali.honrao@gmail.com");
+
+		System.out.println("STEP - Click on Login button");
+		loginPage.clickOnLoginBtn();
+
+		System.out.println("VERIFY - Email required Error message is not visible");
+		boolean emailErrorMessageFlag = loginPage.isEmailRequiredElementDisplayed();
+		Assert.assertFalse(emailErrorMessageFlag);
+
+		System.out.println("VERIFY - Password required Error message is visible");
+		boolean passwordErrorMessageFlag = loginPage.isPasswordRequiredElementDisplayed();
+		Assert.assertTrue(passwordErrorMessageFlag);
+	}
+
+	@Test
+	public void verifyEmailErrorMessageDisplayed() {
+		System.out.println("STEP - Enter valid password");
+		loginPage.enterPassword("Sayali@1441");
+
+		System.out.println("STEP - Click on Login button");
+		loginPage.clickOnLoginBtn();
+
+		System.out.println("VERIFY - Email required Error message is visible");
+		boolean emailErrorMessageFlag = loginPage.isEmailRequiredElementDisplayed();
+		Assert.assertTrue(emailErrorMessageFlag);
+
+		System.out.println("VERIFY - Password required Error message is not visible");
+		boolean passwordErrorMessageFlag = loginPage.isPasswordRequiredElementDisplayed();
+		Assert.assertFalse(passwordErrorMessageFlag);
+	}
+
+	@Test(dataProvider = "LoginDataProvider")
+	public void verifyloginUsingDataDriven(String username, String password, String loginStatus) {
+		System.out.println("STEP - Login with given credentails");
+		loginPage.login(username, password);
+		String currentURL = "";
+		boolean loginFlag;
+		if (loginStatus.equalsIgnoreCase("pass")) {
+			System.out.println("VERIFY - Login Successful toast message displayed");
+			loginFlag = loginPage.isLoginSuccessFullyDisplayed();
+			Assert.assertTrue(loginFlag, "Login successfully Message is not displayed");
+
+			System.out.println("VERIFY - Incorrect email or password message is not displayed");
+			loginFlag = loginPage.isLoginUnSuccessfulElementDisplayed();
+			Assert.assertFalse(loginFlag, "Incorrect email or password message is displayed");
+
+			currentURL = loginPage.getCurrentURL();
+			System.out.println("VERIFY - Application should redirect to dahsboard page");
+			Assert.assertTrue(currentURL.endsWith("dashboard/dash"), "Current URL :" + currentURL);
+		} else {
+			loginFlag = loginPage.isLoginUnSuccessfulElementDisplayed();
+			Assert.assertTrue(loginFlag, "Incorrect email or password message is not displayed");
+
+			loginFlag = loginPage.isLoginSuccessFullyDisplayed();
+			Assert.assertFalse(loginFlag, "Login successfully Message is displayed");
+
+			currentURL = loginPage.getCurrentURL();
+			Assert.assertTrue(currentURL.endsWith("auth/login"));
+		}
+	}
+
+	@DataProvider(name = "LoginDataProvider")
+	public Object[][] getLoginData() throws IOException {
+		return ExcelOperations.getAllRows(".//testData/LoginData.xlsx", "Login");
 	}
 
 	@AfterMethod
